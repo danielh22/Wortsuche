27f955f5885f81aca49183755b2728e9e887e2b8
st166250 <st166250@stud.uni-stuttgart.de>
2025-03-13 14:34:14 +0100

    Rekursion hinzugefügt

--- Wortsuche.cpp
+++ Wortsuche.cpp
@@ -2,51 +2,25 @@
 #include "Wortsuche.h"
 #include <string>
 #include <vector>
-#include <random>
-#include <algorithm>
 
 using namespace std;
 
 int main(void) {
 
-    random_device gen;
-    mt19937 g(gen());
-
-    vector<string> wordList;
-    for (char one = 'a'; one<='z'; ++one){
-        for(char two = 'a'; two<='z'; ++two){
-            for(char three='a'; three<='z'; three++){
-                for(char four='a'; four<='z'; four++){
-                    string word = {one, two, three, four};
-                    wordList.push_back(word);
-                }
-            }
-        }
-    }
-
-    shuffle(wordList.begin(), wordList.end(), gen);
-    
-    int counter = 0;
-    for(auto iterator=wordList.begin(); iterator!=wordList.end(); iterator++){
-        //cout << *iterator << endl;
-        counter++;
-    }
-    cout << "Total words: " << counter << endl;
-
-    vector<string> smallData = {"balu", "balz", "bala", "arfs", "ers", "awd", "q", "kdpe","bal"};
-
+    vector<string> wordList = createWordList();
 
     TrieNode* root = new TrieNode();
-    for (string& s : smallData){
+ 
+    for (string& s : wordList){
         insert(root, s);
     }
-    vector<string> searchKeys = {"bal"};
-    for(string& s : searchKeys){
-        cout << "Searching " << s << endl;
-        if(search(root, s)==true){
-            cout << "FOUND" << endl;
-        }
-        else cout << "NOT FOUND" << endl;
+
+    string searchKey = "BAR";
+
+    vector<string> foundWords = searchPrefixRecursive(root, searchKey);
+    //vector<string> foundWords = searchPrefixNoRecursion(root, searchKey);
+
+    for(string& word : foundWords){
+        cout << word << " ";
     }
-    return 0;
 }
--- Wortsuche.h
+++ Wortsuche.h
@@ -1,13 +1,16 @@
 #include <string>
-#include <iostream>
 #include <vector>
+#include <random>
+#include <algorithm>
+#include <iostream>
+
 #define NUMBER_OF_LETTERS 26
 
 using namespace std;
 
-struct TrieNode {
+struct TrieNode{
     public: 
-        char data;
+        string data;
         TrieNode* children[NUMBER_OF_LETTERS];
         bool EndOfWord;
 
@@ -20,45 +23,109 @@ struct TrieNode {
 
 void insert(TrieNode* root, string& key){
     TrieNode* current = root;
+    string parent_data;
     
     for (int i=0; i<key.length(); ++i){
         char c = key[i];
-        if (current->children[c-'a'] == nullptr){
+        if (current->children[c-'A'] == nullptr){
             TrieNode* newNode = new TrieNode();
-            current->children[c-'a'] = newNode;
+            current->children[c-'A'] = newNode;
         }
-        current = current->children[c-'a'];
-        current->data = c;
+        current = current->children[c-'A'];
+        string inter(1,c);
+        current->data = parent_data + c;
+        parent_data = parent_data + c;
     }
     current->EndOfWord = true;
 }
 
-bool search(TrieNode* root, string& key){
+vector<string> createWordList(){
+    random_device gen;
+    mt19937 g(gen());
+    vector<string> wordList;
+
+
+    for (char one = 'A'; one<='Z'; ++one){
+        for(char two = 'A'; two<='Z'; ++two){
+            for(char three='A'; three<='Z'; three++){
+                for(char four='A'; four<='Z'; four++){
+                    string word = {one, two, three, four};
+                    wordList.push_back(word);
+                }
+            }
+        }
+    }
+    shuffle(wordList.begin(), wordList.end(), gen);
+    return wordList;
+}
+
+
+
+bool search_withoutRec(TrieNode* root, string& key){
     TrieNode* current = root;
+    TrieNode* keyRoot = nullptr;
+    TrieNode* keyRoot2 = nullptr;
+    TrieNode* keyRoot3 = nullptr;
+    TrieNode* keyRoot4 = nullptr;
     vector<TrieNode*> FoundWords;
 
-    if(key.length() == 4){
-    for (int i=0; i<key.length(); ++i){
-        char c = key[i];
-        if (current->children[c-'a'] == nullptr) return false;
-        current = current->children[c-'a'];
+    for(char c : key){
+        if(current->children[c-'A'] == nullptr) return false;
+        current = current->children[c-'A'];
     }
-    return current->EndOfWord;
+    keyRoot = current;
+    if(keyRoot->EndOfWord) FoundWords.push_back(keyRoot);
+    for(char x = 'A'; x<='Z'; x++){
+        if(keyRoot->children[x-'A'] != nullptr){
+            keyRoot2 = keyRoot->children[x-'A'];
+            if(keyRoot2->EndOfWord) FoundWords.push_back(keyRoot2);
+            for(char y = 'A'; y<='Z'; y++){
+                if(keyRoot2->children[y-'A'] != nullptr){
+                    keyRoot3 = keyRoot2->children[y-'A'];
+                    if(keyRoot3->EndOfWord) FoundWords.push_back(keyRoot3);
+                    for(char z = 'A'; z<='Z'; z++){
+                        if(keyRoot3->children[z-'A'] != nullptr){
+                            keyRoot4 = keyRoot3->children[z-'A'];
+                            if(keyRoot4->EndOfWord) FoundWords.push_back(keyRoot4);
+                        }
+                    }
+                }   
+            }   
+        }
     }
-    
-    else if(key.length() < 4){
-        for (int i=0; i<key.length(); ++i){
-            char c = key[i];
-            if(current->children[c-'a'] == nullptr) return false;
-            current = current->children[c-'a'];
-        }
-        for(char x = 'a'; x<='z'; x++){
-            if(current->children[x-'a'] != nullptr) FoundWords.push_back(current->children[x-'a']);
-        }
-        cout << "Found Words: " << endl;
-        for (TrieNode* word : FoundWords){
-            cout << word->data << "_";
-        }
+    cout << "Found Words: " << endl;
+    for (TrieNode* word : FoundWords){
+        cout << word->data << " & ";
     }
     return true;
+}
+
+void searchWordsRecursive(TrieNode* root, vector<TrieNode*>& FoundTrieNodes, string currentPrefix=""){
+    if(root==nullptr) return;
+
+    if(root->EndOfWord == true){
+        FoundTrieNodes.push_back(root);
+    }
+
+    for(char c='A'; c<='Z'; c++){
+        int index = c-'A';
+        if(root->children[index] != nullptr){
+            searchWordsRecursive(root->children[index], FoundTrieNodes, currentPrefix+c);
+        }
+    }
+}
+
+vector<string> searchPrefixRecursive(TrieNode* root, string currentPrefix=""){
+    vector<string> foundWords;
+    vector<TrieNode*> FoundTrieNodes;
+
+    for(char c : currentPrefix){
+        if(root->children[c-'A'] == nullptr) return foundWords;
+        root = root->children[c-'A'];
+    }
+    searchWordsRecursive(root, FoundTrieNodes, currentPrefix);
+    for (TrieNode* word : FoundTrieNodes){
+        foundWords.push_back(word->data);
+    }
+    return foundWords;
 }
